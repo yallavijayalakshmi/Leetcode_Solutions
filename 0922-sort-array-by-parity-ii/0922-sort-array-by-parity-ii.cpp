a83255f99914d5b1b7384e5f4d116b420afe3646
yallavijayalakshmi <yallavijayalakshmi23@gmail.com>
2026-09-11 20:18:51 +0530

    Time: 4 ms (11.21%), Space: 28.4 MB (5.18%) - LeetHub

---
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> even;
+        vector<int> odd;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] % 2 == 0) {
+                even.push_back(nums[i]);
+            }
+            else {
+                odd.push_back(nums[i]);
+            }
+        }
+        vector<int> answer;
+        for(int i = 0; i < n / 2; i++) {
+            answer.push_back(even[i]);
+            answer.push_back(odd[i]);
+        }
+        return answer;
+    }
+};
