357711a933beebdeafbbfb5bc51ad562e49747c6
yallavijayalakshmi <yallavijayalakshmi23@gmail.com>
2026-09-16 06:41:42 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.21%) - LeetHub

---
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0;
+        int j = s.size()-1;
+        while(i<=j){
+            if(!isalpha(s[i])){
+                i++;
+            }
+            else if(!isalpha(s[j])){
+                j--;
+            }
+            else{
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+        }
+        return s;
+    }
+};
